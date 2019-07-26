decc9a801f497be1e2d66adf5949f3b624666dda
cdechery <cdechery@gmail.com>
2019-07-26 18:19:55 +0000

    Some improvements, debug mode

--- rallyslack.py
+++ rallyslack.py
@@ -1,52 +1,65 @@
-#note, you'll need to be running python2 (built with 2.7, python DOES NOT WORK )
-#you'll need to pip install pyral (the python-rally connector) as slacker (the slack connector)
+#!/usr/bin/env python
 
+import sys
 from datetime import datetime
 from datetime import timedelta
 from pyral import Rally
 from slacker import Slacker
 
-slack = Slacker('your slack key')
+DEBUG = False
+if len(sys.argv) > 1:
+	if sys.argv[1] == '-debug' or sys.argv[1] == '--debug':
+		DEBUG = True
 
-# Send a message to #integration-testing channel
+
+slack = Slacker('xxxxxx')
 
 server="rally1.rallydev.com"
 
 #as we are using an API key, we can leave out the username and password
-user=""
-password=""
+user="xxx"
+password="xxxxx"
 
-workspace="Your Workspace"
-project="Your Project"
-apikey="Your API Key"
+workspace="workspace NAME, not ID"
+project="project NAME, not ID"
+#apikey="xxxxxxx"
 
 #which slack channel does this post to?
-channel = "#softwareteam"
+channel = "#rally"
 
 #Assume this system runs (via cron) every 15 minutes.
-interval = 15 * 60
+interval = 60 * 60
 
 #format of the date strings as we get them from rally
 format = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 #create the rally service wrapper
-rally = Rally(server, user, password, apikey=apikey, workspace=workspace, project=project)
-
+rally = Rally(server, user, password, workspace=workspace, project=project)
 
 #build the query to get only the artifacts (user stories and defects) updated in the last day
 querydelta = timedelta(days=-1)
-querystartdate = datetime.utcnow() + querydelta;
+querystartdate = datetime.utcnow() + querydelta
 query = 'LastUpdateDate > ' + querystartdate.isoformat()
+
+tags_to_exclude = ['some','tags','toignore']
 
 response = rally.get('Artifact', fetch=True, query=query, order='LastUpdateDate desc')
 for artifact in response:
+
+	for exclude_tag in tags_to_exclude:
+		if exclude_tag in artifact.Tags:
+			continue
+
 	include = False
 
 	#start building the message string that may or may not be sent up to slack
 	postmessage = '*' + artifact.FormattedID + '*'
-	postmessage = postmessage + ': ' + artifact.Name + '\n';
-	for revision in artifact.RevisionHistory.Revisions: 
+	owner = artifact.CreatedBy.DisplayName.replace('[AD] ', '')
+	postmessage = postmessage + ': ' + artifact.Name + ' (Owner: '+owner+') \n'
+
+	for revision in artifact.RevisionHistory.Revisions:
 		revisionDate = datetime.strptime(revision.CreationDate, format)
+		formated_date = revisionDate.strftime('%d/%m/%Y %H:%M:%S')
 		age = revisionDate - datetime.utcnow()
 		seconds = abs(age.total_seconds())
 		#only even consider this story for inclusion if the timestamp on the revision is less than iterval seconds onld
@@ -59,9 +72,12 @@ for artifact in response:
 				#the only kinds of updates we care about are changes to OWNER and SCHEDULE STATE
 				#other changes, such as moving ranks around, etc, don't matter so much
 				if item.startswith('SCHEDULE STATE ') or item.startswith("OWNER added "):
-					postmessage = postmessage  + "> " + item + ' \n';
+					postmessage = postmessage  + "> " + formated_date+' '+item + ' \n'
 					include = True
 
 	if include:
 		postmessage = postmessage + 'https://rally1.rallydev.com/#/search?keywords=' + artifact.FormattedID + '\n'
-		slack.chat.post_message(channel=channel, text=postmessage, username="rallyslackbot", as_user=False)
+		if DEBUG:
+			print(postmessage)
+		else:
+			slack.chat.post_message(channel=channel, text=postmessage, username="rallyslackbot", as_user=False)
